7db74b7d0a1f4da226c26ae0baefccd9c9824e69
Patrik Sigl <Sigl.Patrik@stud.vassboskovice.cz>
2022-03-22 11:35:09 +0100

    :))))))

--- auto/MainWindow.xaml.cs
+++ auto/MainWindow.xaml.cs
@@ -20,15 +20,69 @@ namespace auto
     /// </summary>
     public partial class MainWindow : Window
     {
+        Vehicle auto1;
+        Vehicle auto2;
+
         public MainWindow()
         {
             InitializeComponent();
-            Vehicle auto1 = new Vehicle("Škoda");
-
+            auto1 = new Vehicle("Škoda");
+            ShowVehicle(auto1, txt1);
             auto1.Type = "Osobní Auto";
 
-            Vehicle auto2 = new Vehicle("W");           
+            auto2 = new Vehicle("W");           
             auto2.Type = "Dodávka";
+            ShowVehicle(auto2, txt2);
+        }
+
+        public void ShowVehicle(Vehicle vehicle, TextBox textbox)
+        {
+            textbox.Text = $"brand: {vehicle.Brand}\n";
+            textbox.Text += $"Type: {vehicle.Type}\n";
+            textbox.Text += $"Fuel: {vehicle.Fuel}\n";
+            textbox.Text += $"EngineVolume: {Math.Round(vehicle.EngineVolume), 2}\n";
+            textbox.Text += $"FuelLevel: {vehicle.FuelLevel}\n";
+            textbox.Text += $"Distance: {vehicle.Distance}\n";
+            textbox.Text += $"PneuState: {Math.Round(vehicle.PneuState), 2}\n";
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            auto1.Move();
+            
+            ShowVehicle(auto1, txt1);
+            
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            auto2.Move();
+            ShowVehicle(auto2, txt2);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            auto1.Refuel();
+            ShowVehicle(auto1, txt1);
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            auto1.TireChange();
+            ShowVehicle(auto1, txt1);
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            auto1.DistanceReset();
+            ShowVehicle(auto1, txt1);
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            auto1.EngineRepair();
+            ShowVehicle(auto1, txt1);
         }
     }
 }
--- auto/Vehicle.cs
+++ auto/Vehicle.cs
@@ -12,7 +12,7 @@ namespace auto
         public double EngineVolume { get; set; }
         public int FuelLevel { get; set; }
         public int Distance { get; set; }
-        public int PneuState { get; set; }
+        public double PneuState { get; set; }
         
 
         public Vehicle(string brand)
@@ -24,6 +24,35 @@ namespace auto
             FuelLevel = 100;
             Distance = 0;
             PneuState = 100;
+        }
+        public void Move()
+        {
+            EngineVolume -= 0.4;
+            FuelLevel -= 1;
+            if (FuelLevel < 0)
+            {
+                FuelLevel = 0;
+                
+            }
+            Distance += 10;
+            PneuState -= 0.1;
+        }
+        public void Refuel()
+        {
+            FuelLevel = 100;
+        }
+        
+        public void TireChange()
+        {
+            PneuState = 100;
+        }
+        public void DistanceReset()
+        {
+            Distance = 0;
+        }
+        public void EngineRepair()
+        {
+            EngineVolume = 1000;
         }
     }
 }
